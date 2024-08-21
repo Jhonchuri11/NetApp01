f20366f97d038c9812b2214e1a2639e86a467048
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-08-20 22:55:35 -0500

    add new class Rectangular

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,6 +16,8 @@ namespace ConsoleApp1
 
 
             // Definiendo dos variables
+
+            /*
             int area = 0;
             int perimetro = 0;
 
@@ -54,9 +56,41 @@ namespace ConsoleApp1
                 Codigo = 1000
             };
 
+            */
+
+            Rectangular rectangular = new Rectangular
+            {
+                x1 = 1, y1 = 1,
+                x2 = 1, y2 = 4,
+                x3 = 5, y3 = 4,
+                x4 = 5, y4 = 1  
+            };
+            rectangular.CalcularBase();
+            rectangular.CalcularAltura();
+            rectangular.CalcularArea();
+            rectangular.CalcularPerimetro();
+
+            Console.WriteLine(rectangular.Base);
+            Console.WriteLine(rectangular.Altura);
+            Console.WriteLine(rectangular.Area);
+            Console.WriteLine(rectangular.Perimetro);
+
+
+
             Console.Read();
 
+            // Base 1 en horizontal
+            // (0,8) (4,8)
+           
+            // 8
+            // Base 2 en horizontal
 
+            // (0,0) (4,0)
+            // 4
+
+            // Base = 4
+            // Altura = 8
+            // Area = 32
         }
     }
 }
---
+++ ConsoleApp1/Rectangular.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Rectangular
+    {
+
+        public int x1 { get; set; }
+        public int x2 { get; set; }
+        public int x3 { get; set; }
+        public int x4 { get; set; }
+
+        public int y1 { get; set; }
+        public int y2 { get; set; }
+        public int y3 { get; set; }
+        public int y4 { get; set; }
+
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public double Distancia {  get; set; }
+
+        public double Area { get; set; }
+
+        public double Perimetro { get; set; }
+
+        public double CalcularDistancia(int a1, int b1, int a2, int b2) 
+        {
+            return Distancia = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+
+        }
+        public void CalcularBase()
+        {
+            Base = CalcularDistancia(x1, y1, x2, y2);
+        }
+
+        public void CalcularAltura()
+        {
+            Altura = CalcularDistancia(x2, y2, x3, y3);
+        }
+
+        public void CalcularArea()
+        {
+            Area = Base * Altura;
+        }
+        public void CalcularPerimetro()
+        {
+            Perimetro = (Base + Altura) * 2; 
+        }
+    }
+}
